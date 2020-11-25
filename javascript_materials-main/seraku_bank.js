562b2abcc73deebf7e58719b4c07867062229d29
boababy <70796898+boababy@users.noreply.github.com>
2020-11-25 15:13:15 +0900

    Add files via upload

--- javascript_materials-main/seraku_bank.js
+++ javascript_materials-main/seraku_bank.js
@@ -127,11 +127,26 @@ var hides=document.getElementsByClassName("content")[0].getElementsByTagName("li
             this.className="select";
             hides[this.index].className="";
         }
-        
-       /* tabs[i].onmouseout=function(){
-            for(var i =1;i<tabs.length;i++){
-                this.className="";
-            }
-            hides[this.index].className="show";
-        }*/
     }
+
+
+
+/*POPUP*/
+  $(function () {
+    $('.openModal').click(function() {  //openModalをクリックした時に
+      var btnIndex = $(this).index(); 
+      //何番目のモーダルボタンかを取得
+      
+      $('.modalArea').eq(btnIndex).addClass('is-open'); 
+      //クリックしたモーダルボタンと同じ番目のモーダルを表示する。addClassでis-openクラスを追加して表示する
+  　　　　$('html, body').css('overflow', 'hidden'); 
+      // overflow:hidden;の追加で背景のスクロール禁止に
+    });
+    $('.closeModal, .modalBg').click(function() { //closeModalかmodalBgをクリックした時に
+      $('.modalArea').removeClass('is-open'); 
+      //モーダルを非表示にする。removeClassでis-openクラスを削除して非表示にする
+  　　　　$('html, body').removeAttr('style'); 
+      //追加したoverflow:hidden;を削除
+    });
+  
+  });
