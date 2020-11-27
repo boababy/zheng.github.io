9e3dd2a385025cc9ee6e4f5bc45c36f90eb9157c
boababy <70796898+boababy@users.noreply.github.com>
2020-11-27 11:17:10 +0900

    Add files via upload

--- javascript_materials-main/login/login.js
+++ javascript_materials-main/login/login.js
@@ -4,10 +4,11 @@ $(document).ready(function(){
         var password=$('#password').val();
         var error=true;
         
-
         $.ajax({
-            type:"POST",
+            type:"GET",
             url:"data.json",
+            async:true,    //是否异步
+            jsonp: "callbackparam",    //跨域请求的参数名，默认是callback
             dataType:"json",
             success:function(data){
                 $('#loading').fadeOut('fast');
@@ -17,19 +18,26 @@ $(document).ready(function(){
                     }
                 });
                 if(error==false){
+                    alert('success!');
                     document.location="save_ss.php?user_login=" +user;
+                    //document.location="www.google.com"
                 }else{
-                    $('#container').slideUp('slow').slideDown('slow');
+                    
+                    //$('#container').slideUp('slow').slideDown('slow');
                     $('#user').val('');
                     $('#password').val('');
                     alert('ユーザー名或いはパスワードが間違います。'); 
                 }
 
-            }
+            },
+            //error: function(data) {
+            //    alert('error');
+            //}
         });
-        
-         return false; 
-
-        
+        return false; 
     });
 });
+
+
+/*
+        */
--- javascript_materials-main/login/save_ss.php
+++ javascript_materials-main/login/save_ss.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    echo $_GET['callbackparam']."(".$Data.")";
     if(isset($_GET['user_login'])){
         $user=$_GET['user_login'];
         $_SESSION['user']=$user;
