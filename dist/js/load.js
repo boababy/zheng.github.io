bd0a985e001c5bfdfd5a60e0feb5d3c84ff3dacc
boababy <70796898+boababy@users.noreply.github.com>
2020-12-01 12:46:39 +0900

    Add files via upload

---
+++ dist/js/load.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $.getJSON("include.json",function(data){
+        var employee_data="";
+        $.each(data,function(key,value){
+            employee_data+='<li class="user-box col-md-2 col-sm-4 col-xs-12 text-center">';
+            employee_data+='<div class="thumbnail">';
+            employee_data+='<div class="thumbnail-image">';
+            employee_data+='<img class="img-circle" src="'+value.imagePath+'">';
+            employee_data+='</div>';
+            employee_data+='<div class="user-indicator"></div>';
+            employee_data+='<div class="user-name">'+value.lastName+value.firstName+'</div>';
+            employee_data+='<div class="user-division">'+value.division+'</div>';
+            employee_data+='<div class="user-position">'+value.position+'</div>';
+            employee_data+='<div class="text-center"></div>';
+            employee_data+='</div>';
+            employe_data+='</li>';
+        });
+        console.log(employee_data);
+        $('#loadarea').append(employee_data);
+
+    });
+});
+
+
+
+/*
+<li class="user-box col-md-2 col-sm-4 col-xs-12 text-center">
+            <div class="thumbnail">
+              <div class="thumbnail-image">
+                <img class="img-circle" src="dist/img/jinbutsu_male.jpg">
+              </div>
+              <div class="user-indicator"></div>
+              <div class="user-name">日比野定男</div>
+              <div class="user-division">WEB</div>
+              <div class="user-position">主査</div>
+              <div class="text-center"></div>
+            </div>
+</li>
+*/
