b4aecf6f2ad7aa199fdfa3a538d8f06b41a73036
boababy <70796898+boababy@users.noreply.github.com>
2020-12-02 15:22:41 +0900

    Add files via upload

--- dist/js/graph.js
+++ dist/js/graph.js
@@ -18,7 +18,6 @@ $(document).ready(function(){
                     "#FAFF67",
                 ],
                 data: [male.length,female.length]
-
             }],
             // これらのラベルは凡例とツールチップに表示されます。
             labels: [
@@ -26,7 +25,6 @@ $(document).ready(function(){
                 '女'
             ]
         };
-
        // グラフを作成
        var ctx = document.getElementById("stage");
        var PieChart = new Chart(ctx, {
@@ -39,6 +37,58 @@ $(document).ready(function(){
             }
           }
         });
+    });
+});
 
+
+//役職者の割合
+$(document).ready(function(){
+    $.getJSON("dist/js/include.json", function (data) {
+        var syusa = $.grep(data, function (n, i) {
+            return n.position == '主査'
+        });
+        console.log(syusa.length)
+
+        var katyou= $.grep(data, function (n, i) {
+            return n.position == '課長'
+        });
+
+        console.log(katyou.length)
+
+        var dairi= $.grep(data, function (n, i) {
+            return n.position == '課長代理'
+        });
+
+        console.log(dairi.length)
+
+
+        var danjyoData={
+            datasets: [{
+                backgroundColor: [
+                    "#FAFF67",
+                    "#58A27C",
+                    "#3C00FF"
+                ],
+                data: [syusa.length,katyou.length,dairi.length]
+            }],
+            // これらのラベルは凡例とツールチップに表示されます。
+            labels: [
+                '主査',
+                '課長',
+                '課長代理'
+            ]
+        };
+       // グラフを作成
+       var ctx2 = document.getElementById("stage2");
+       var PieChart2 = new Chart(ctx2, {
+           type: 'pie', 
+           data: danjyoData,
+           options: {
+            title: {
+              display: true,
+              text: '役職者比'
+            }
+          }
+        });
     });
 });
