b098dbee711a5b52a38792a4c1ac01dfd7a02985
boababy <70796898+boababy@users.noreply.github.com>
2020-12-02 15:00:25 +0900

    Add files via upload

--- dist/js/graph.js
+++ dist/js/graph.js
@@ -1,63 +1,3 @@
-
-/*
-$(document).ready(function(){
-    $.getJSON("dist/js/include.json",function(results){
-        console.log(results);
-
-        //男女のデータをdanjyoに格納
-        var danjyo=[];
-        for(var sex in results)
-        {
-            danjyo.push(results[sex].gender);
-        }
-        console.log(danjyo);
-
-        var female=[];
-        var male=[];
-
-        if(danjyo.value=="女"){
-            female=danjyo.length
-        }
-        console.log(female);
-
-        if(danjyo.value=="男"){
-            male=danjyo.length
-        }
-        console.log(male);
-        
-
-        //円グラフを作成
-        var ctx = document.getElementById('stage').getContext('2d');
-        var chart = new Chart(ctx, {
-        type: 'pie',
- 
-        // データを指定
-        data: {
-         labels:["女","男"],
-         datasets: [{
-             label: '男女比',
-             borderColor:['rgb(75, 192, 192)','rgb(255, 99, 132)'],
-             fill: false,
-             data: female,male,
-         }]
-        },
- 
-        // 設定はoptionsに記述
-        options: {
-        //タイトル
-         title: {
-         display: true,
-         text: '線グラフの例'
-            }   
-        }
-        });
-
-    });
-});
-*/
-
-
-
 $(document).ready(function(){
     $.getJSON("dist/js/include.json", function (data) {
         var male = $.grep(data, function (n, i) {
@@ -71,17 +11,19 @@ $(document).ready(function(){
 
         console.log(female.length)
 
-        //男女数量
-        //  male.length  female.length
-
         var danjyoData={
             datasets: [{
+                backgroundColor: [
+                    "#BB5179",
+                    "#FAFF67",
+                ],
                 data: [male.length,female.length]
+
             }],
             // これらのラベルは凡例とツールチップに表示されます。
             labels: [
-                'Red',
-                'Blue'
+                '男',
+                '女'
             ]
         };
 
@@ -93,7 +35,7 @@ $(document).ready(function(){
            options: {
             title: {
               display: true,
-              text: '男女割合'
+              text: '男女比'
             }
           }
         });
